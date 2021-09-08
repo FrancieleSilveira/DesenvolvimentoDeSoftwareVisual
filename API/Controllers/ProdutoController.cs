e1135c5aff44159ae62239c20105f3c7777abe97
Franciele Silveira <franciele.silveira1603@gmail.com>
2021-09-08 15:31:38 -0300

    CRUD de Produto com IActionResult

--- API/Controllers/ProdutoController.cs
+++ API/Controllers/ProdutoController.cs
@@ -17,19 +17,61 @@ namespace API.Controllers
             _context = context;
         }
 
-        //POST: api/produtp/create
+        //POST: api/produto/create
         [HttpPost]
         [Route("create")]
-        public Produto Create(Produto produto)
+        public IActionResult Create([FromBody] Produto produto)
         {
             _context.Produtos.Add(produto);
             _context.SaveChanges();
-            return produto;
+            return Created("", produto);
         }
 
-        //GET: api/produtp/list
+        //GET: api/produto/list
         [HttpGet]
         [Route("list")]
-        public List<Produto> List() => _context.Produtos.ToList(); // ToList pega todos os registros da tabela de Produtos e converter para uma lista de produtos
+        public IActionResult List() => Ok(_context.Produtos.ToList()); // ToList pega todos os registros da tabela de Produtos e converter para uma lista de produtos
+
+        //GET: api/produto/getbyid/1
+        [HttpGet]
+        [Route("getbyid/{id}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            Produto produto = _context.Produtos.Find(id);
+            if (produto != null) return NotFound();
+            return Ok(produto);
+        }
+
+        // DELETE: api/produto/delete/bolacha
+        [HttpDelete]
+        [Route("delete/{name}")]
+        public IActionResult Delete([FromRoute] string name)
+        {
+            //Buscar um objeto na tabela de produtos com base no nome
+            Produto produto = _context.Produtos.FirstOrDefault
+            (
+                //Expressão Lambda
+                //x => x.Nome == nome && x.Preco > 10
+                produto => produto.Nome == name
+            );
+            if (produto == null)
+            {
+                return NotFound();
+            }
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+            return Ok();
+            //return Ok(_context.Produtos.ToList()); retorna a lista atualizada
+        }
+
+        //PUT: api/produto/update
+        [HttpPut]
+        [Route("update")]
+        public IActionResult Update([FromBody] Produto produto)
+        {
+            _context.Produtos.Update(produto);
+            _context.SaveChanges();
+            return Ok(produto);
+        }
     }
 }
